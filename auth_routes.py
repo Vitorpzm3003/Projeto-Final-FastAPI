b781c90146facbe0bcf1864091f3ae8867a6ae49
Vitorpzm3003 <vitorpzmelo@gmail.com>
2026-05-24 19:34:27 -0300

    adição das rotas padrões

--- auth_routes.py
+++ auth_routes.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
+
+@auth_router.get("/")
+async def authenticate():
+    """
+    Essa é a rota padrão de autenticação
+    """
+    return {"mensage": "Você acesso a rota de autenticação", "auth":False}
--- order_routes.py
+++ order_routes.py
@@ -1,3 +1,11 @@
 from fastapi import APIRouter
 
 order_router = APIRouter(prefix="/order", tags=["order"])
+
+@order_router.get("/")
+async def getList():
+    """
+    Essa é a rota padrao de pedidos.  Todas as rotas de orders precisam de autenticação
+    """
+    return {"mensagem": "Lista de pedidos"}
+
